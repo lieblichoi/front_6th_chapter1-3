5ccb23e7e75f17fa70638af5db09d866c177f6b1
Jaehwan Choi <lieblichoi@gmail.com>
2025-07-24 23:25:50 +0900

    Feature: useAutoCallback 훅 구현

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -1,7 +1,15 @@
-import type { AnyFunction } from "../types";
-import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
+import type { AnyFunction } from "../types";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const fnRef = useRef(fn);
+  fnRef.current = fn;
+
+  const memoizedFn = useRef<T>();
+
+  if (!memoizedFn.current) {
+    memoizedFn.current = ((...args: Parameters<T>): ReturnType<T> => fnRef.current(...args)) as T;
+  }
+
+  return memoizedFn.current as T;
 };
